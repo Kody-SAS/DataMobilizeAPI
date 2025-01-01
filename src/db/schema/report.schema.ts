9d90d3f7bc00838b413961e022e5fe0f23729827
Asam <abbasaliaboubakar@gmail.com>
2025-01-01 10:13:49 +0100

    remove enums

--- src/db/schema/report.schema.ts
+++ src/db/schema/report.schema.ts
@@ -3,22 +3,6 @@ import { timestamps } from "./helpers.schema";
 import { v7 as uuid } from "uuid";
 import { users } from "./user.schema";
 
-const roadTypeEnum = [
-  "Section",
-  "Intersection",
-  "Station",
-  "Bridge",
-  "Other",
-] as const;
-
-const categoryEnum = [
-  "Safety Perception",
-  "Crash Reporting",
-  "Infrastructure Issue",
-  "Audit",
-  "Other",
-] as const;
-
 export const reports = pgTable("reports", {
   id: text("id")
     .$defaultFn(() => uuid())
@@ -27,8 +11,8 @@ export const reports = pgTable("reports", {
     .unique(),
   description: text("description").notNull(),
   localisation: varchar("localisation", { length: 255 }).notNull(),
-  roadType: text("road_type", { enum: roadTypeEnum }).notNull(),
-  category: text("category", { enum: categoryEnum }).notNull(),
+  roadType: text("road_type"),
+  category: text("category"),
   photos: jsonb("photos").notNull(),
   userId: text("userId").references(() => users.id),
   ...timestamps,
