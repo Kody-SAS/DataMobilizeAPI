98ad6bd9a47fc9d884787e04c7c9fc14357ea3ef
monkeyK1n9 <mkeyk1n9@gmail.com>
2025-06-16 14:26:23 +0200

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,18 +2,22 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 import {
   DATABASE,
+  DATABASE_URL,
   HOST,
   PORT,
   POSTGRES_PASSWORD,
   POSTGRES_PORT,
   POSTGRES_USER,
 } from "./src/startup/config";
+
+const isProduction = process.env.NODE_ENV == 'production' ? true : false;
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema",
   dialect: "postgresql",
   dbCredentials: {
-    url: `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${HOST}:${POSTGRES_PORT}/${DATABASE}`,
+    url: isProduction ? DATABASE_URL : `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${HOST}:${POSTGRES_PORT}/${DATABASE}`,
   },
   migrations: {
     table: "my-migrations-table", 
