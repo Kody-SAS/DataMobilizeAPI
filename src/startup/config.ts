1165ab951a26d997dbd84349fdcaee4c932a31d1
monkeyK1n9 <mkeyk1n9@gmail.com>
2025-06-29 14:34:29 +0200

    update the google ID Client

--- src/startup/config.ts
+++ src/startup/config.ts
@@ -33,9 +33,7 @@ export const emailHttpClient = axios.create({
 });
 
 // google
-export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-export const GOOGLE_WEB_CLIENT=process.env.GOOGLE_WEB_CLIENT;
+export const GOOGLE_ID_CLIENT = process.env.GOOGLE_ID_CLIENT;
 export const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
 export const CALLBACK_URL = process.env.CALLBACK_URL;
 export const DATABASE_URL = process.env.DATABASE_URL;
